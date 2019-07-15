f030ac7d65f0419953cc66dc0c72b1c4c8563b68
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-07-15 16:41:20 +0300

    Adding styles for download mobile.

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,13 +1,18 @@
-var navMenu = document.querySelector(".main-nav");
+
+var header = document.querySelector(".page-header");
+var navMenu = header.querySelector(".main-nav");
 var toggleNav = navMenu.querySelector(".main-nav__toggle-btn");
 
+header.classList.add("page-header--transparent");
 navMenu.classList.remove("main-nav--nojs");
 
 toggleNav.addEventListener("click", function() {
   if (navMenu.classList.contains("main-nav--closed")) {
     navMenu.classList.remove("main-nav--closed");
+    header.classList.remove("page-header--transparent");
     navMenu.classList.add("main-nav--opened");
   } else {
+    header.classList.add("page-header--transparent");
     navMenu.classList.add("main-nav--closed");
     navMenu.classList.remove("main-nav--opened");
   }
