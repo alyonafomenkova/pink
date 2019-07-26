582d540f1b69eb19925a2af9b73a1a2a0a9b25f0
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-07-26 20:02:20 +0300

    Adding slider for tariffs.

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,30 +1,57 @@
 var reviewsBlock = document.querySelector(".reviews");
-var slides = document.querySelectorAll(".slider__item");
-var currentSlide = document.querySelector(".slider__item--current");
-var sliderToggles = document.querySelectorAll(".slider__toggle");
-var currentSliderToggle = document.querySelector(".slider__toggle--current");
+var reviewSlides = reviewsBlock.querySelectorAll(".slider__item");
+var reviewCurrentSlide = reviewsBlock.querySelector(".slider__item--current");
+var reviewToggles = reviewsBlock.querySelectorAll(".slider__toggle");
+var reviewCurrentToggle = reviewsBlock.querySelector(".slider__toggle--current");
+var tariffsBlock = document.querySelector(".tariffs");
+var tariffsTable = tariffsBlock.querySelector(".tariffs__table");
+var tariffToggles = tariffsBlock.querySelectorAll(".slider__toggle");
+var tariffCurrentToggle = tariffsBlock.querySelector(".slider__toggle--current");
 
- TODO: // rewrite dotsSlider and arrowSlider using classes
+TODO: // rewrite dotsSlider and arrowSlider using classes
 
 reviewsBlock.classList.remove("reviews--nojs");
 reviewsBlock.classList.add("reviews--js");
 
-for (var i = 0; i < sliderToggles.length; i++) {
-  sliderToggles[i].addEventListener("click", function(evt) {
+for (var i = 0; i < reviewToggles.length; i++) {
+  reviewToggles[i].addEventListener("click", function (evt) {
     evt.preventDefault();
 
     if (!this.classList.contains("slider__toggle--current")) {
-      currentSliderToggle.classList.remove("slider__toggle--current");
-      currentSliderToggle = this;
+      reviewCurrentToggle.classList.remove("slider__toggle--current");
+      reviewCurrentToggle = this;
       this.classList.add("slider__toggle--current");
 
-      for (var j = 0; j < slides.length; j++) {
-        if (sliderToggles[j].classList.contains("slider__toggle--current")) {
-          currentSlide.classList.remove("slider__item--current");
-          slides[j].classList.add("slider__item--current");
-          currentSlide = slides[j];
+      for (var j = 0; j < reviewSlides.length; j++) {
+        if (reviewToggles[j].classList.contains("slider__toggle--current")) {
+          reviewCurrentSlide.classList.remove("slider__item--current");
+          reviewSlides[j].classList.add("slider__item--current");
+          reviewCurrentSlide = reviewSlides[j];
         }
       }
     }
   })
 }
+
+function showTariffs() {
+  for (var i = 0; i < tariffToggles.length; i++) {
+    tariffToggles[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      if (!evt.target.classList.contains("slider__toggle--current")) {
+        tariffCurrentToggle.classList.remove("slider__toggle--current");
+        tariffCurrentToggle = evt.target;
+        evt.target.classList.add("slider__toggle--current");
+
+        if (evt.target.classList.contains("slider__toggle--first")) {
+          tariffsTable.style.transform = "translateX(20px)";
+        } else if (evt.target.classList.contains("slider__toggle--second")) {
+          tariffsTable.style.transform = "translateX(-260px)";
+        } else if (evt.target.classList.contains("slider__toggle--third")) {
+          tariffsTable.style.transform = "translateX(-540px)";
+        }
+      }
+    });
+  }
+};
+
+showTariffs();
